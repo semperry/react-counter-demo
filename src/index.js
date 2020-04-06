b1af6d0090e5c418cf80ee178f50bd0e31621f88
Ryan Curtis <49254281+semperry@users.noreply.github.com>
2020-04-06 17:24:25 -0600

    console.log in index
    added console log to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-
+console.log("next push");
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
